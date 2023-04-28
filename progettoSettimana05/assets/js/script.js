b48a55b5cec47545368b6f1caf18f1b121ede921
Walter Barbieri <128515233+uolli90@users.noreply.github.com>
2023-04-29 00:10:51 +0200

    WalterProgettoWeek05CommitFinaleVeramente

--- progettoSettimana05/assets/js/script.js
+++ progettoSettimana05/assets/js/script.js
@@ -29,29 +29,3 @@ window.addEventListener("scroll", function () {
       btn.style.backgroundColor = "#191919";
     }
   });
-
-  findOverflowParents(document.querySelector('#discoverMore'));
-  function findOverflowParents(element, initEl) {
-
-    function notVisible(el) {
-      let overflow = getComputedStyle(el).overflow;
-      return overflow !== "visible";
-    }
-  
-    function displayFlex(el) {
-      let display = getComputedStyle(el).display;
-      return display === "flex";
-    }
-  
-    let thisEl = element;
-    if (!initEl) console.log('** Overflow check commence!', thisEl);
-    let origEl = initEl || thisEl;
-    if (notVisible(thisEl)) console.warn("Overflow found on:", thisEl.tagName, { issue: "OVERFLOW IS NOT VISIBLE", tagName: thisEl.tagName, id: thisEl.id, class: thisEl.className, element: thisEl });
-    if (displayFlex(thisEl)) console.warn("Flex found on:", thisEl.tagName, { issue: "DISPLAY IS FLEX", tagName: thisEl.tagName, id: thisEl.id, class: thisEl.className, element: thisEl });
-    if (thisEl.parentElement) {
-      return findOverflowParents(thisEl.parentElement, origEl);
-    } else {
-      return console.log('** Overflow check complete! original element:', origEl);
-    }
-  
-  }
